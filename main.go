0237fe82385e4a531b34b8e706493b08738493dd
MoustafaHaroun <Moustafa_Haroun@outlook.com>
2024-07-13 14:21:19 +0200

    feat: finish chapter one

--- main.go
+++ main.go
@@ -2,10 +2,36 @@ package main
 
 import "fmt"
 
-func Hello(name string) string {
-	return "Hello, " + name + "!"
+const spanish = "Spanish"
+const french = "French"
+const dutch = "Dutch"
+
+const spanishHello = "Hola"
+const englishHello = "Hello"
+const frenchHello = "Bonjour"
+const dutchHello = "Hallo"
+
+func Hello(name, language string) string {
+	if name == "" {
+		name = "World"
+	}
+
+	return greetingPrefix(language) + ", " + name
+}
+
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case spanish:
+		return spanishHello
+	case french:
+		return frenchHello
+	case dutch:
+		return dutchHello
+	default:
+		return englishHello
+	}
 }
 
 func main() {
-	fmt.Println(Hello("Moustafa"))
+	fmt.Println(Hello("Moustafa", ""))
 }
